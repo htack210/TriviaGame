ab2ef5af0793105efe0054edc4e6f441817e4a4a
htack210 <glenn.hline@outlook.com>
2024-04-29 10:42:35 -0400

    next commit

---
+++ components/FullCard.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+export function FullCard({
+  questionObject,
+  handleNextQuestion,
+  handleFlipped,
+  flippedState,
+}) {
+  return (
+    // <div onClick={() => handleFlipped()}>
+    <div>
+      {/* Card Front */}
+      {!flippedState && (
+        <>
+          <div className="card">
+            <div className="top">
+              {/* <img src="../public/vite.svg" /> */}
+              <p>
+                <b>{questionObject.category}</b>
+                <br />
+                {questionObject.question}
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* Card back */}
+      {flippedState && (
+        <>
+          <div className="card-back">
+            <p>{questionObject.ans}</p>
+            <button onClick={() => handleNextQuestion()}>
+              Click to continue
+            </button>
+          </div>
+        </>
+      )}
+      <div className="responseBtn-nav" onClick={() => handleFlipped()}>
+        <button className="responseBtn">A. {questionObject.A}</button>
+        <button className="responseBtn">B. {questionObject.B}</button>
+        <button className="responseBtn">C. {questionObject.C}</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import nflTriviaData from "../components/NflTrivia";
+import { FullCard } from "../components/FullCard.jsx";
+const categories = ["All", "Teams", "Players & Coaches"];
+const questions = nflTriviaData;
+
+function App() {
+  const [flipped, setFlipped] = useState(false); //Not flipped at first
+  const [filteredBtns, setFilteredBtns] = useState(questions);
+  const [questionIdx, setQuestionIdx] = useState(0);
+
+  const handleClick = (category) => {
+    console.log(category);
+    if (category === "All") {
+      setFilteredBtns(questions); // These are questions from ALL categories.
+    } else {
+      const filteredQuestions = questions.filter(
+        (btn) => btn.category === category
+      );
+      setFilteredBtns(filteredQuestions); // These are questions filtered by a single category.
+    }
+  };
+
+  function handleFlipped() {
+    setFlipped(!flipped);
+    console.log(flipped);
+  }
+
+  function nextIndex() {
+    if (questionIdx < filteredBtns.length - 1) {
+      setQuestionIdx(questionIdx + 1);
+    } else {
+      setQuestionIdx(0);
+      // This should kick off final score calculation.
+    }
+
+    setFlipped(false);
+  }
+  return (
+    <>
+      <div className="main-nav">
+        <h1>NFL Trivia</h1>
+        <p>Think you know the NFL? Let's find out!</p>
+      </div>
+      <div className="category-nav">
+        {categories.map((cat) => {
+          return (
+            <button
+              className="categoryButton"
+              key={cat}
+              onClick={() => handleClick(cat)}
+            >
+              {cat}
+            </button>
+          );
+        })}
+      </div>
+
+      <div className="content">
+        <div className="cards-container">
+          {console.log(filteredBtns[questionIdx])}
+          <FullCard //Child to app.jsx
+            questionObject={filteredBtns[questionIdx]} //Sends filteredBtns object to child.
+            handleNextQuestion={nextIndex} // Increments index number to move app to next question.
+            handleFlipped={handleFlipped} // Card flip function.
+            flippedState={flipped} // Shows card flipped state.
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
